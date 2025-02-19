2eb2ff7027b1b0ad383b13fe30e1373fab5939c4
arnaldojrsantos <arnaldosantosjr01@gmail.com>
2025-02-19 13:17:35 -0300

    Aspas e strings
    Python = Linguagem de programação
    Tipo e tipagem  =  Dinâmica / Forte
    str -> string -> texto
    String são textos que estão dentro de aspas

---
+++ aula03.py
@@ -0,0 +1,28 @@
+'''
+Python = Linguagem de programação
+Tipo e tipagem  =  Dinâmica / Forte
+str -> string -> texto
+String são textos que estão dentro de aspas
+'''
+print(1234)
+
+# Aspas simples
+print('Arnaldo Leão')
+
+'''forma mais adequada de imprimri áspas duplas
+no texto.
+'''
+print('Seu nome é "Arnaldo Leão"')
+
+
+# Aspas duplas
+print("Arnaldo Leão")
+
+
+#Scape
+print("Arnaldo \"Leão\"")  # essa é a forma de inserir Áspas na string
+   
+
+# r
+# essa é a forma de exibir o escape (no caso, as barras invertidas)
+print(r"Arnaldo \"Leão\"")  
