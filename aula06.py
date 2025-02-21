cdec6047dc529cf9a4cac832444f7ad65780ed8a
arnaldojrsantos <arnaldosantosjr01@gmail.com>
2025-02-21 16:59:05 -0300

    Create aula006.py
    Conversão de tipos primitivos

---
+++ aula06.py
@@ -0,0 +1,18 @@
+# conversão de tipos, coerção
+# type convertion, typecasting, coercion
+# é o ato de converter um tipo em outro
+#   tipos imutáveis e primitivos:
+# str, int, float, bool
+
+'''Convertenedo str para int'''
+print(int('1'), type(int('1'))) 
+print(int('1') +  1)
+
+'''Convertendo str para float'''
+print(float('1.3') +  1)
+
+'''Convertendo str para bool'''
+print(bool(' ')) # True
+
+'''Convertendo para str'''
+print(str(11) + 'b')
