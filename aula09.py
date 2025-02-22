e902e8a67eff30ead6dd698a95e0bf6d60178bf9
Arnaldo Santos <83240835+arnaldosantosjr@users.noreply.github.com>
2025-02-22 20:44:33 -0300

    Upadate 09.py
    introdução a operadores aritméticos

---
+++ aula09.py
@@ -0,0 +1,27 @@
+#introdução a operadores aritiméticos
+adicao = 10 + 10
+print('Adição', adicao)
+
+subtracao = 10 - 5
+print('Subtração', subtracao)
+
+multiplicacao = 10 * 10
+print('Multiplicação', multiplicacao)
+
+divisao = 10 / 2.2 #float
+print('Disão', divisao)
+
+divisao_interia = 10 // 2.2
+print('Divisão Inteira', divisao_interia)
+
+potenciacao = 2 **10
+print('Potenciação', potenciacao)
+
+modulo =  55 % 2 #resto da divisão
+print('Módulo', modulo)
+
+print(10 % 8 == 0) # Vevificando se um número é multiplo de outro.Também serve para saber se é par ou ímpar
+print(16 % 8 == 0) 
+print(10 % 2 == 0) 
+print(15 % 2 == 0) 
+print(16 % 2 == 0) 
