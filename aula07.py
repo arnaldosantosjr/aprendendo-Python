21e6c77b682b7915c390dfafff08e1bb0c6fc32b
Arnaldo Santos <83240835+arnaldosantosjr@users.noreply.github.com>
2025-02-21 21:27:52 -0300

    Crate aula07.py
    Introdução a variáveis

---
+++ aula07.py
@@ -0,0 +1,18 @@
+# Variáveis são usadas para salvar algo na memória do computador.
+# PEP8: indice variáveis com letras minúsculas, pode usar numeros  e underline _.
+# sinal de = é o operrador de atrinuição. Ele é usado para atribuir um valor a um nome (variável).
+# Uso: nome_variável = expressão
+
+
+# nome_completo = 'Arnaldo Santos Junior'
+# soma_dois_mais_dois = 2 + 2
+# int_um = bool('1')
+# print(int_um, type(int_um)) 
+# print(nome_completo, soma_dois_mais_dois) 
+
+
+nome = 'Arnaldo'
+idade = 10
+maior_de_idade = idade >= 18
+print('Nome: ', nome, 'Idade', idade)
+print('É maior ?', maior_de_idade)
