4dd58517fb8597c69b10e379571e289c3df29efb
Arnaldo Santos <83240835+arnaldosantosjr@users.noreply.github.com>
2025-02-23 17:02:03 -0300

    Create aula10.py
    Concatenação(+) e repetição(*) com operadores aritméticos

---
+++ aula10.py
@@ -0,0 +1,8 @@
+# Concatenação(+) e repetição(*) com operadores aritméticos
+concatenacao = 'Arnaldo' +str(1) +' ' + 'Santos'
+print(concatenacao)
+
+a_dez_vezes = 'A ' *10
+tres_vezes_arnaldo = 3* 'Arnaldo'
+print(a_dez_vezes)
+print(tres_vezes_arnaldo)
