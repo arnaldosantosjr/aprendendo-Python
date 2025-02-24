60aa1b810a3c026acaed2a026bc9021cbac298d9
Arnaldo Santos <83240835+arnaldosantosjr@users.noreply.github.com>
2025-02-24 19:55:55 -0300

    Create aula012.py
    Calculadora de iimc

---
+++ aula012.py
@@ -0,0 +1,9 @@
+#Calculadore de imc
+nome = 'Arnaldo Leão'
+altura = 1.81
+peso = 63
+imc =  64/ 1.81**2 
+
+print(nome, 'tem',altura,'de altura,' )
+print('pesa', peso,'kg  e seu IMC é:')
+print(imc)
