0e427b38b45f86a23ca073ff5c36ccb6980d1a86
Arnaldo Santos <83240835+arnaldosantosjr@users.noreply.github.com>
2025-02-26 19:49:56 -0300

    Create aula18.py
    Debugger do VS Code e o interpretador do python lendo os códigos

---
+++ aula18.py
@@ -0,0 +1,22 @@
+# Debugger do VS Code e o interpretador do python lendo os códigos
+# entendendo fluxo de do interpretador em condicionais
+
+condicao1 = False
+condicao2 = False
+condicao3 = True
+condicao4 = True
+
+if condicao1 :
+    print('Condição 1')
+elif condicao2:
+    print('Condição 2')
+elif condicao3:
+    print('Condição 3')
+else:
+    print('Condição 4')
+
+
+if 10 == 10:
+    print('Outro if')
+
+print('Fora do if')
