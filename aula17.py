189b459565543f0105d68c798711fff8e174fd16
arnaldojrsantos <arnaldosantosjr01@gmail.com>
2025-02-26 13:43:28 -0300

    Create aula17.py
    entendendo fluxo de do interpretador em condicionais

---
+++ aula17.py
@@ -0,0 +1,23 @@
+# if / elife....../.else
+# se / se não se / se não
+# entendendo fluxo de do interpretador em condicionais
+
+condicao1 = False
+condicao2 = True
+condicao3 = False
+condicao4 = True
+
+if condicao1 :
+    print('Condição 1')
+elif condicao2:
+    print('Condição 2')
+elif condicao3:
+    print('Condição 3')
+else:
+    print('Condição 4')
+
+
+if 10 == 10:
+    print('Outro if')
+
+print('Fora do if')
