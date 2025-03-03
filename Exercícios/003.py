cd693af326b4d3a842d712c670a79a3e90de1fcf
Arnaldo Santos <83240835+arnaldosantosjr@users.noreply.github.com>
2025-03-03 20:41:24 -0300

    Create 003.py

---
+++ Exercícios/003.py
@@ -0,0 +1,15 @@
+def buscaMenor(arr):
+    menor = arr[0]
+    menor_indice = 0
+    for i in range(1, len(arr)):
+        if arr[i] < menor:
+            menor = arr[i]
+            menor_indice = i
+    return menor_indice
+def ordenacaoporSecao(arr):
+    novoArr = []
+    for i in range(len(arr)):
+        menor = buscaMenor(arr)
+        novoArr.append(arr.pop(menor))
+    return novoArr
+print([5, 3, 6, 2, 10])
