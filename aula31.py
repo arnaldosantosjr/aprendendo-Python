e9684075587ac980f082278a739968ff5f72c391
Arnaldo Santos <83240835+arnaldosantosjr@users.noreply.github.com>
2025-03-06 17:32:26 -0300

    Create aula31.py
    id - A identidade do valor que está na memória

---
+++ aula31.py
@@ -0,0 +1,17 @@
+"""
+Falag (Bandeira) - Marcar um local
+None = Não valor
+Is e is not = é ou não é (tipo, valor. identidade)
+id = Identidade
+"""
+
+v1 = 'a'
+v2 = 'b'
+print(id(v1))
+print(id(v2))
+# condição = False
+
+# if condicao:
+#       print('Faça algo')
+# else:
+#       print('Não faça algo')
