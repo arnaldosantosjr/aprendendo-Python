8d5e478d5350bd542cecef0e33113142452ad7ce
Arnaldo Santos <83240835+arnaldosantosjr@users.noreply.github.com>
2025-03-10 21:09:17 -0300

    Upedate 008c.py
    inserindo a variavel tamanho_nome

--- Exercícios/008c.py
+++ Exercícios/008c.py
@@ -2,11 +2,16 @@
 Faça um progema que peça o primeiro nome do usuário. Se o nome tiver 4 letras ou menos escreva : "Seu nome é curto."; se tiver entre 5 e 6 letras, escreva: "Seu nome é normal."; Se maior que 6 letras: "Seu nome é grande".'''
 
 nome = input('Infome o seu primeiro nome. \n')
-print(len(nome))
 
-if len(nome) <= 4:
-    print(f'O nome "{nome}" é muito curto')
-elif len(nome) == 6 or len(nome) == 5:
-    print(f'O nome "{nome}" é normal ')
+tamanho_nome = len(nome)
+
+if tamanho_nome >1:
+
+    if len(nome) <= 4:
+        print(f'O nome "{nome}" é muito curto')
+    elif len(nome) == 6 or len(nome) == 5:
+        print(f'O nome "{nome}" é normal ')
+    else:
+        print(f'O nome "{nome}" é grande')
 else:
-    print(f'O nome "{nome}" é grande')
+    print('Digite uma letra')
