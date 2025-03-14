c29bbe40c6ea073106e0b7b4646d1512ff6996e5
arnaldojrsantos <arnaldosantosjr01@gmail.com>
2025-03-14 16:51:22 -0300

    Update 009laco.py
    Inclusão do input dentro do laço

--- Exercícios/009laco.py
+++ Exercícios/009laco.py
@@ -2,6 +2,8 @@ nome = input('Informe seu nome. \n')
 
 while not nome:
     print('Você não informou seu nome')
+    nome = input('Informe seu nome.\n')
 if nome == True:
     print(f'Seu nome é{nome}')
-    break
+    
+    
