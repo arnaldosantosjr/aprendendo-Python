f4c353dc23af4dc34ea23c4e18a119720b6625e3
arnaldojrsantos <arnaldosantosjr01@gmail.com>
2025-03-14 16:35:56 -0300

    Create aula35.py
    Loop com condição e detalhes

---
+++ aula35.py
@@ -0,0 +1,15 @@
+'''
+Repetições
+while (enquanto)
+Executar uma ação enquanto uma condição for verdadeira
+Loop infinito -> Quando não tem fim
+'''
+contador = 0
+
+while contador <=10:
+    # Se trocar o print de lugar com o contator =  contador +1 
+    # O inicio da contagem muda.
+    print(contador)
+    contador = contador + 1
+
+print('Acabou')
