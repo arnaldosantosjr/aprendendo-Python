8856583ae7604ff00c334cbfd8edf9adfaf257af
Arnaldo Santos <83240835+arnaldosantosjr@users.noreply.github.com>
2025-03-14 22:20:57 -0300

    Create aula36.py
    Operadores de atribuição

---
+++ aula36.py
@@ -0,0 +1,22 @@
+'''
+Operadores de atribuição
+= += -= *= /= **= %=
+'''
+contador0 = 10
+contador1 = 10
+contador2 = 10
+contador3 = 10
+contador4 = 10
+contador5 = 10
+#########
+
+
+contador0 +=2 ## mesma coisa de contador = contador + 1
+contador1 -=2
+contador2 *=2
+contador3 /=2
+contador4 **=2
+contador5 %=2
+
+print(contador0, contador1, contador2)
+print(contador3, contador4, contador5)
