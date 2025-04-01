f0d27d7ac19224b478f313487ea62ccdd93201d6
Arnaldo Santos <83240835+arnaldosantosjr@users.noreply.github.com>
2025-04-01 20:13:30 -0300

    Updade and renam aula41.py, and crate aula43.py
    Uso do for para laços que si quantas vezes irão se repetir

---
+++ aula43.py
@@ -0,0 +1,27 @@
+#Uso do for
+# senha_salva = '123456'
+# senha_digitada = ''
+# repeticoes = 0
+
+# while senha_salva != senha_digitada:
+#     senha_digitada = input(f'Seua senha ({repeticoes}x): ')
+
+#     repeticoes += 1
+
+# print(repeticoes)
+# print('Aquele laço acima pode ter repetições infinitas')
+
+
+texto = 'Python'
+
+novo_texto = ''
+for letra in texto:
+    novo_texto += f'*{letra}'
+    print(letra)
+print(novo_texto + '*')
+# i = 0
+# tamnaho_str =len(texto)
+# while i < tamnaho_str:
+#     print(texto[i], i)
+
+#     i += 1
