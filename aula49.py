0f5521772e469d5a477dd4d43fff317675cd9590
arnaldojrsantos <arnaldosantosjr01@gmail.com>
2025-04-23 15:44:15 -0300

    Create aula49.py
    for in com lista

---
+++ aula49.py
@@ -0,0 +1,7 @@
+# for in com listas
+
+lista = ['Arnaldo', 'Tony','Junior']
+
+for nome in lista :
+    print(nome) # Exibe cada nome da lista
+    print(nome, type(nome)) # Exibe o tipo de cada nome da lista
