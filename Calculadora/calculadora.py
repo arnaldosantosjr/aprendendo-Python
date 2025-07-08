8d2176fb62886513fb84656806a5f0cfd20918db
Arnaldo Santos <arnaldosantosjr01@gmail.com>
2025-07-08 16:15:20 -0300

    Update calculadora.py
    Foi corrigido o erro na interface, ode os botões de subtração, soma e igualdade estavam sendo cortados.

--- Calculadora/calculadora.py
+++ Calculadora/calculadora.py
@@ -12,46 +12,51 @@ class Calculator:
         self.input_text = tk.StringVar()
 
         # Display da calculadora
-        self.input_frame = tk.Frame(master, bd=0, relief="flat", bg="#eee")
-        self.input_frame.pack(side=tk.TOP)
+        self.input_frame = tk.Frame(master, bd=0, relief="flat", bg="#eee") # << Crie o frame primeiro
+        self.input_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
-        self.input_field = tk.Entry(self.input_frame, font=('arial', 18, 'bold'),
+        self.input_field = tk.Entry(self.input_frame, font=('arial', 18, 'bold'), # << Crie o Entry depois do frame
                                     textvariable=self.input_text, width=20, bg="#eee",
-                                    bd=0, justify=tk.RIGHT, state='readonly') # 'readonly' para impedir digitação direta
-        self.input_field.grid(row=0, column=0, ipady=10) # ipady para padding interno
+                                    bd=0, justify=tk.RIGHT, state='readonly')
+        self.input_field.grid(row=0, column=0, ipady=10, sticky="nsew") # << Agora configure-o
+        self.input_frame.grid_columnconfigure(0, weight=1) # << E configure o peso da coluna do frame
 
         # Frame para os botões
         self.btns_frame = tk.Frame(master, bg="#ccc")
-        self.btns_frame.pack()
+        self.btns_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+        for i in range(4): # Temos 4 colunas de botões (0 a 3)
+            self.btns_frame.grid_columnconfigure(i, weight=1)
+        for i in range(5): # Temos 5 linhas de botões (0 a 4)
+            self.btns_frame.grid_rowconfigure(i, weight=1)
 
         # Linhas de botões
         # Linha 1: C, /, *, -
-        self.create_button("C", 1, 0, command=self.clear_all, bg="#f00", fg="white")
-        self.create_button("/", 1, 1, command=lambda: self.button_click("/"), bg="#f7f7f7")
-        self.create_button("*", 1, 2, command=lambda: self.button_click("*"), bg="#f7f7f7")
-        self.create_button("-", 1, 3, command=lambda: self.button_click("-"), bg="#f7f7f7")
+        self.create_button("C", 0, 0, command=self.clear_all, bg="#f00", fg="white")
+        self.create_button("/", 0, 1, command=lambda: self.button_click("/"), bg="#f7f7f7")
+        self.create_button("*", 0, 2, command=lambda: self.button_click("*"), bg="#f7f7f7")
+        self.create_button("-", 0, 3, command=lambda: self.button_click("-"), bg="#f7f7f7")
 
         # Linha 2: 7, 8, 9, +
-        self.create_button("7", 2, 0, command=lambda: self.button_click("7"))
-        self.create_button("8", 2, 1, command=lambda: self.button_click("8"))
-        self.create_button("9", 2, 2, command=lambda: self.button_click("9"))
-        self.create_button("+", 2, 3, command=lambda: self.button_click("+"), rowspan=2, bg="#f7f7f7")
+        self.create_button("7", 1, 0, command=lambda: self.button_click("7"))
+        self.create_button("8", 1, 1, command=lambda: self.button_click("8"))
+        self.create_button("9", 1, 2, command=lambda: self.button_click("9"))
+        self.create_button("+", 1, 3, command=lambda: self.button_click("+"), rowspan=2, bg="#f7f7f7")
 
         # Linha 3: 4, 5, 6
-        self.create_button("4", 3, 0, command=lambda: self.button_click("4"))
-        self.create_button("5", 3, 1, command=lambda: self.button_click("5"))
-        self.create_button("6", 3, 2, command=lambda: self.button_click("6"))
+        self.create_button("4", 2, 0, command=lambda: self.button_click("4"))
+        self.create_button("5", 2, 1, command=lambda: self.button_click("5"))
+        self.create_button("6", 2, 2, command=lambda: self.button_click("6"))
 
         # Linha 4: 1, 2, 3
-        self.create_button("1", 4, 0, command=lambda: self.button_click("1"))
-        self.create_button("2", 4, 1, command=lambda: self.button_click("2"))
-        self.create_button("3", 4, 2, command=lambda: self.button_click("3"))
+        self.create_button("1", 3, 0, command=lambda: self.button_click("1"))
+        self.create_button("2", 3, 1, command=lambda: self.button_click("2"))
+        self.create_button("3", 3, 2, command=lambda: self.button_click("3"))
 
         # Linha 5: 0, ., =
-        self.create_button("0", 5, 0, command=lambda: self.button_click("0"), columnspan=2)
-        self.create_button(".", 5, 2, command=lambda: self.button_click("."))
-        self.create_button("=", 4, 3, command=self.evaluate_expression, rowspan=2, bg="#5cb85c", fg="white")
-
+        self.create_button("0", 4, 0, command=lambda: self.button_click("0"), columnspan=2)
+        self.create_button(".", 4, 2, command=lambda: self.button_click("."))
+        self.create_button("=", 3, 3, command=self.evaluate_expression, rowspan=2, bg="#5cb85c", fg="white")
 
     def create_button(self, text, row, column, command, bg="#fff", fg="black", rowspan=1, columnspan=1):
         button = tk.Button(self.btns_frame, text=text, fg=fg, bg=bg,
